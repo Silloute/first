f51f2c2e95e19eec02942f7c4a14d2ec387a71e7
silloute <2256001763@qq.com>
2020-11-18 17:13:37 +0800

    第一次

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <audio controls>
+  <source :src=m type="audio/mpeg">
+  您的浏览器不支持 audio 元素。
+</audio>
+    <button @click="btnclick">获取</button>
+  </div>
+</template>a
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data(){
+    return {
+      m:{
+        type:String,
+        default:""
+      }
+    }
+  },
+  methods: {
+    btnclick() {
+      function request(config) {
+        //检测当前环境是否为生产环境
+        const instance = axios.create({
+          //由于有跨域问题，所以调试和打包用不同的baseURL
+          baseURL: "https://api.imjad.cn/cloudmusic/",
+          // baseURL: "https://autumnfish.cn/",
+          timeout: 5000,
+        });
+        return instance(config);
+      }
+      request({
+        url: "?type=search&search_type=1&s=cocoon",
+        // withCredentials: true
+      }).then(res=>{
+      // this.m = res.data.data[0].url;
+      console.log(res);
+      });
+
+    },
+  },
+};
+</script>
+
+<style>
+</style>
